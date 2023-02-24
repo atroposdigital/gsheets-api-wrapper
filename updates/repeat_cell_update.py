23228404bb68bb397f41e7dce29993b69844ebc2
SiGm4 <dimgmx@hotmail.com>
2023-02-24 18:14:25 +0200

    Built field_mask_string_to_dict

--- updates/repeat_cell_update.py
+++ updates/repeat_cell_update.py
@@ -7,49 +7,63 @@ class RepeatCellUpdate:
         self._construct_cell_data()
     
     def _construct_cell_data(self):
-        self._field_mask_string_to_json(self.fields)
+        self._field_mask_string_to_dict(self.fields)
 
-    def _field_mask_string_to_json(self, field_mask_string):
-        field_mask_json = {}
-        current = field_mask_json
+    def _field_mask_string_to_dict(self, field_mask_string):
+        def _separate_transitive(parts):
+            reconstruct = []
+            reconstruct_flag = False
+            reconstruct_index = 0
+            for i in range(len(parts) - 1, -1, -1):
+                part = parts[i]
 
-        parts = field_mask_string.split(".")
+                if ")" in part:
+                    reconstruct_flag = True
+                    reconstruct.append([])
 
-        for i in range(len(parts)):
-            part = parts[i]
-            if "(" in part:
-                parts[i] = part.split('(')
-            
+                if reconstruct_flag:
+                    reconstruct[reconstruct_index].insert(0, part.replace(")",""))
+                    parts.pop(i)
 
-        print (parts)
+                if "(" in part:
+                    reconstruct_flag = False
+                    reconstruct_index += 1
+            return parts, reconstruct
 
-        # for i in range(len(parts)):
-        #     part = parts[i]
-        #     #next_part = parts[i+1]
-        #     if "(" in parts[i]:
-        #         key, value = part.split('(')
-        #         current[key] = {}
-        #         current = current[key]
-        #         value = value.split(')')[0].split(',')
-        #         for subpart in value:
-        #             current[subpart] = None
-                
-        #         print(field_mask_json)
-        #         print(value)
+        def _flatten_reconstruct_list(reconstruct):
+            for i in range(len(reconstruct)): 
+                reconstruct[i] = reconstruct[i][0].split("(") + reconstruct[i][1:]
+            return reconstruct
+        
+        def _apply_transitive(parts, reconstruct):
+            for i in range(len(reconstruct)):
+                key = reconstruct[i][0]
+                for y in range(1, len(reconstruct[i])):
+                    parts.append(key + "." + reconstruct[i][y])
+            return parts
 
-        # print(parts)
-        # print("\n")
+        def _add_parts_to_dict(parts):
+            field_mask_dict = {}
 
-        # for part in parts:
-        #     if '(' in part:
-        #         key, value = part.split('(')
-        #         current[key] = {}
-        #         current = current[key]
-        #         subparts = value.rstrip(')').split(',')
-        #         for subpart in subparts:
-        #             current[subpart] = None
-        #     else:
-        #         current[part] = {}
-        #         current = current[part]
+            for i in range(len(parts)):
+                current = field_mask_dict
+                subparts = parts[i].split('.')
+                for y in range(len(subparts)):
+                    key = subparts[y]
+                    if key not in field_mask_dict.keys():
+                        current[key] = {}
+                    if y == len(subparts) - 1:
+                        current[key] = None
+                    current = current[key]
 
-        # print(field_mask_json)
+            return field_mask_dict
+        
+        print(field_mask_string)
+
+        parts = field_mask_string.split(",")
+        parts, reconstruct = _separate_transitive(parts)
+        reconstruct = _flatten_reconstruct_list(reconstruct)
+        parts = _apply_transitive(parts, reconstruct)
+        self.cell = _add_parts_to_dict(parts)
+
+        print(self.cell)
