bf9fc037e8fa4b5e313df39629ba2173202ebe0b
atroposdigital <dimitris@atroposdigital.com>
2024-05-03 20:50:45 +0300

    Corrected package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-setup(name = 'gsheets-api-wrapper',
+setup(name = 'gsheets_api_wrapper',
    version = '0.0.1',
    description = 'A wrapper for the Google API',
    package_dir = {"": "app"},
